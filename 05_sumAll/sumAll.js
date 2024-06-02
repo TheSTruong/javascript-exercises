899bdb9f2b28be8f8690fcf8fcf7d8dbe41a08bc
TheSTruong <struongdev@gmail.com>
2024-06-02 01:43:45 -0600

    Exercise 5-7 Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    for (let arg of args) {
+        if (typeof arg !== 'number' || arg < 0)
+            return "ERROR";
+    }
+    
+    let min = Math.min(...args);
+    let max = Math.max(...args);
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
