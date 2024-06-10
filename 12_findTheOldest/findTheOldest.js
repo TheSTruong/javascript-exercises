20cad021fbe9cfd6459841e77c6350c410711077
TheSTruong <svtruong@ualberta.ca>
2024-06-09 20:21:24 -0600

    Exercise 11-12 Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+function findAge(deathYear, birthYear) {
+    if (!deathYear) {
+        return new Date().getFullYear() - birthYear;
+    }
+    return deathYear - birthYear;
+}
+
+const findTheOldest = function (array) {
+    let persons = array.reduce((oldest, nextPerson) => {
+        let oldestAge = findAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        let personAge = findAge(nextPerson.yearOfDeath, nextPerson.yearOfBirth);
+        return oldestAge > personAge ? oldest : nextPerson;
+    })
+
+    return persons
 
 };
 
