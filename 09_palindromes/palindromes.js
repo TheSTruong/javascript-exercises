8b10d135adbe347019c4023d4c5588d02c09e6df
TheSTruong <svtruong@ualberta.ca>
2024-06-09 18:14:06 -0600

    Exercise 9 Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let stringFiltered = string.toLowerCase().split("").filter((char) => validChars.includes(char)).join("");
+    let filteredReversed = stringFiltered.split('').reverse().join('');
+    return stringFiltered === filteredReversed;
 };
 
 // Do not edit below this line
