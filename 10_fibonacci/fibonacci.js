855b5ce76ff38c0101e5cc80bdd5582188776e5f
TheSTruong <svtruong@ualberta.ca>
2024-06-09 18:35:25 -0600

    Exercise 10 Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let number = num;
+    if (typeof number !== 'number') {
+        number = parseInt(number);
+    };
+    if (number < 0) return "OOPS";
+    let fibArray = [0, 1];
+    for (let i = 2; i <= number; i++) {
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    return fibArray[number];
 
-};
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
